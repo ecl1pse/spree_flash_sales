e6a000619036971d434db571987dde94d987fada
ecl1pse <ecl1pse.armageddon@gmail.com>
2013-06-25 04:53:37 -0700

    All specs pass! Victory!

--- spec/features/flash_sale_spec.rb
+++ spec/features/flash_sale_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 feature "Flash Sale for a taxon" do
-  let(:flash_sale) { create(:flash_sale) }
+  given!(:flash_sale) { create(:flash_sale) }
+  given!(:product) { flash_sale.saleable.products.first }
 
   background do
     visit spree.flash_sale_path(flash_sale)
@@ -12,14 +13,26 @@ feature "Flash Sale for a taxon" do
   end
 
   scenario "shows a list of products" do
-    expect(page).to have_css(".product", count: 3)
+    expect(page).to have_css(".flash-sale-product", count: flash_sale.saleable.products.size)
   end
 
   scenario "has a link to view a product" do
     expect(page).to have_link(product.name)
   end
 
+  scenario "link to product goes to product" do
+    find_link(product.name).click
+    current_path.should == spree.product_path(product) # should go to the product directly
+  end
+
+end
+
+feature "Flash Sale Page: no products" do
+  given!(:flash_sale) { create(:flash_sale) }
+
   scenario "has no products" do
+    flash_sale.saleable.products.each(&:destroy)
+    visit spree.flash_sale_path(flash_sale)
     expect(page).to have_content("There doesn't seem to be any products available")
   end
 end
