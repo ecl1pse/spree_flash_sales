3615e506ef9bb5b8556fc030c0d18d46adc7f668
ecl1pse <ecl1pse.armageddon@gmail.com>
2013-06-27 14:21:20 -0700

    Add coveralls

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@ source 'https://rubygems.org'
 
 # Provides basic authentication functionality for testing parts of your engine
 gem 'spree_auth_devise', github: "spree/spree_auth_devise", branch: "2-0-stable"
+gem 'coveralls', require: false
 
 gemspec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,9 @@ require 'database_cleaner'
 require 'ffaker'
 require 'shoulda-matchers'
 
+require 'coveralls'
+Coveralls.wear!
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
