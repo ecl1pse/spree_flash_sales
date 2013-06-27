f99ed685797ef09e1d15e50edb22d1b81197d895
ecl1pse <ecl1pse.armageddon@gmail.com>
2013-06-27 15:35:59 -0700

    Require creating record before adding image. Removed the invoke rewrites and modfied the method for #location_after_save

--- app/controllers/spree/admin/flash_sales_controller.rb
+++ app/controllers/spree/admin/flash_sales_controller.rb
@@ -5,26 +5,16 @@ class Spree::Admin::FlashSalesController < Spree::Admin::ResourceController
     end
   end
 
-  def update
-    invoke_callbacks(:update, :before)
-    if @object.update_attributes(params[object_name])
-      invoke_callbacks(:update, :after)
-      flash[:success] = flash_message_for(@object, :successfully_updated)
-      respond_with(@object) do |format|
-        format.html { redirect_to edit_object_url(@object) }
-      end
-    else
-      invoke_callbacks(:update, :fails)
-      respond_with(@object)
-    end
-  end
-  
   def saleables
     search = params[:saleable_type].constantize.search(:name_cont => params[:name])
     render :json => search.result.map(&:name)
   end
 
   protected
+    def location_after_save
+      edit_object_url(@object)
+    end
+
     def collection
       return @collection if @collection.present?
       if request.xhr? && params[:q].present?
