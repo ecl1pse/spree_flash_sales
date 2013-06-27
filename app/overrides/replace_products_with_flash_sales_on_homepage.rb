dd1766d3550faee10aad0e61f4e0ea454b088077
ecl1pse <ecl1pse.armageddon@gmail.com>
2013-06-27 16:41:59 -0700

    Semantic view and rewrote logic a bit. Will try presenter pattern soon.

--- app/overrides/replace_products_with_flash_sales_on_homepage.rb
+++ app/overrides/replace_products_with_flash_sales_on_homepage.rb
@@ -2,7 +2,7 @@ Deface::Override.new(:virtual_path => 'spree/home/index',
   :name => 'replace_products_with_flash_sales',
   :replace => "[data-hook='homepage_products']",
   :text => "
-    <div data-hook='homepage_flash_sales'>
+    <div class='flash-sales' data-hook='homepage_flash_sales'>
       <%= render partial: 'spree/flash_sales/flash_sale', collection: @flash_sales %>
     </div>
     <% if @flash_sales.blank? %>
