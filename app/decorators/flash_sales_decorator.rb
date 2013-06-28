9d39ac09c5040b0b9d834401f1a1d47bbd451f37
ecl1pse <ecl1pse.armageddon@gmail.com>
2013-06-27 17:21:35 -0700

    Failing

---
+++ app/decorators/flash_sales_decorator.rb
@@ -0,0 +1,19 @@
+class FlashSalesDecorator < Draper::Decorator
+  delegate_all
+
+  def hey_there
+    "hey there"
+  end
+
+  # Define presentation-specific methods here. Helpers are accessed through
+  # `helpers` (aka `h`). You can override attributes, for example:
+  #
+  #   def created_at
+  #     helpers.content_tag :span, class: 'time' do
+  #       object.created_at.strftime("%a %m/%d/%y")
+  #     end
+  #   end
+
+end
+
+
