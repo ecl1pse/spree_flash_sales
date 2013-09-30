387a677f404813f2ff016cea1c34ef2aa5d70d7c
ecl1pse <ecl1pse.armageddon@gmail.com>
2013-09-29 23:32:58 -0700

    Rails 4 and Spree 2-1 support.

--- app/overrides/admin_flash_sales_tab.rb
+++ app/overrides/admin_flash_sales_tab.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
             :name => "admin_flash_sales_tab",
-            :virtual_path => "spree/admin/shared/menu",
-            :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
+            :virtual_path => "spree/admin/shared/_menu",
+            :insert_bottom => "[data-hook='admin_tabs']",
             :text => "<%= tab :flash_sales, :icon => 'icon-th-list' %>"
 )
